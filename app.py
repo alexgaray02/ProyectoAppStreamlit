41f734b8983ed288cf2710dd6755d63b863ce54c
alexgaray02 <agaray@unitru.edu.pe>
2026-04-09 12:49:41 -0500

    actualización de filtros y graficar

--- app.py
+++ app.py
@@ -245,7 +245,7 @@ if archivo is not None:
             # Una vista más limpia para el resumen
             st.plotly_chart(
                 fig_fusion,
-                use_container_width=False,
+                use_container_width=False,   ######........de True a False..........####
                 config={
                     "toImageButtonOptions": {
                         "format": "png",
