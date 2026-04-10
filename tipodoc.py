4179192458b2cb1b9cc4b5af938f2afb06d5f606
alexgaray02 <agaray@unitru.edu.pe>
2026-04-10 10:30:07 -0500

    actualización de filtros y gráficos

--- tipodoc.py
+++ tipodoc.py
@@ -1,13 +1,38 @@
+# import pandas as pd
+
+# def documento(archivo):
+#     df = pd.read_excel(archivo)
+#     cols= [col.strip().lower() for col in df.columns] #Limpiar espacios y pasar a minúsculas
+
+#     if "mes" in cols:
+#         tipo = "AM" #año-mes
+#     else:
+#         tipo = "A" #año
+    
+#     return df, tipo
+
+
+
+######################
 import pandas as pd
 
 def documento(archivo):
-    df = pd.read_excel(archivo)
-    cols= [col.strip().lower() for col in df.columns] #Limpiar espacios y pasar a minúsculas
 
-    if "mes" in cols:
-        tipo = "AM" #año-mes
+    # Si ya es DataFrame (viene de Drive)
+    if isinstance(archivo, pd.DataFrame):
+        df = archivo
+
+    # Si es archivo (upload o ruta)
     else:
-        tipo = "A" #año
-    
+        df = pd.read_excel(archivo)
+
+    # Limpiar columnas
+    cols = [col.strip().lower() for col in df.columns]
+
+    # Detectar tipo
+    if "mes" in cols:
+        tipo = "AM"  # año-mes
+    else:
+        tipo = "A"   # año
+
     return df, tipo
-    
